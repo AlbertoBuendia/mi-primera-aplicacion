b38e83e8811229b24a14dfa72c95c9d063670f8a
AlbertoBuendia <aebuendia@estud.tesa.edu.ec>
2026-02-19 01:29:21 -0500

    inicio

---
+++ android/app/src/main/java/mi/primera/aplicaacion/MainActivity.java
@@ -0,0 +1,5 @@
+package mi.primera.aplicaacion;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'mi.primera.aplicaacion',
+  appName: 'mi-primera-aplicaacion',
+  webDir: 'dist'
+};
+
+export default config;
